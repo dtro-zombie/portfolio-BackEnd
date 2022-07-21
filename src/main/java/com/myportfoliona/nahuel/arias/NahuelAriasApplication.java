807673f42ca84f3868001c8f26fac3e380c8d6b6
nahuel arias <53919868+dtro-zombie@users.noreply.github.com>
2022-07-21 15:39:11 -0300

    empezando backed

---
+++ src/main/java/com/myportfoliona/nahuel/arias/NahuelAriasApplication.java
@@ -0,0 +1,13 @@
+package com.myportfoliona.nahuel.arias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NahuelAriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NahuelAriasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myportfoliona/nahuel/arias/NahuelAriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myportfoliona.nahuel.arias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NahuelAriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
