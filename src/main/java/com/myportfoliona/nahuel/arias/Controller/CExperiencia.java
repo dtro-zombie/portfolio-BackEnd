104517e8ea21195e96b8e1ada4bc452f818f6329
nahuel arias <53919868+dtro-zombie@users.noreply.github.com>
2022-08-10 23:11:30 -0300

    experiencia terminado

--- src/main/java/com/myportfoliona/nahuel/arias/Controller/CExperiencia.java
+++ src/main/java/com/myportfoliona/nahuel/arias/Controller/CExperiencia.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,11 +32,21 @@ public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
     
+   
+    
     @GetMapping("/lista")
     public ResponseEntity<List<Experiencia>> list()
     {
         List<Experiencia> list=sExperiencia.list();
         return new ResponseEntity(list, HttpStatus.OK);
+    }
+    
+     @GetMapping("/detail/{id}")
+    public ResponseEntity<Experiencia> getById(@PathVariable("id") int id){
+        if(!sExperiencia.existsById(id))
+            return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
+        Experiencia experiencia = sExperiencia.getOne(id).get();
+        return new ResponseEntity(experiencia, HttpStatus.OK);
     }
     
     @PostMapping("/create")
@@ -77,7 +88,7 @@ public class CExperiencia {
             
             
             }         
-
+  @DeleteMapping("/delete/{id}") 
     public ResponseEntity<?> delete(@PathVariable("id") int id)
     {
         if(!sExperiencia.existsById(id))
